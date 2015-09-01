6dbb83d16f575275e4f9040086e8c7b1d6f36b2f
Tim Backus <tbackus127@gmail.com>
2015-08-31 23:33:01 -0400

    Commented everything and cleaned up a bit. Brute-force seems to be the quicker method of arriving at this answer.

--- Problem001/Euler1.java
+++ Problem001/Euler1.java
@@ -1,5 +1,6 @@
 //===========================================================================================
 // Project Euler - Problem 1
+// Tim Backus - tbackus127@gmail.com
 //-------------------------------------------------------------------------------------------
 // Multiples of 3 and 5
 // Find the sum of all the multiples of 3 or 5 below 1000.
@@ -9,9 +10,14 @@ import java.util.HashSet;
 
 public class Euler1 {
   
-  // And NOT including
-  private static final int UPPER_LIMIT = 10;
+  // Upper limit (and not inclusive) for the multiples sum.
+  private static final int UPPER_LIMIT = 1000;
   
+  /**
+   * Main method. Executes the two methods of finding the sum of all multiples of 3 and 5, 
+   * and outputs the result and how long it took to compute.
+   * @param args runtime arguments (not used).
+   */
   public static void main(String[] args) {
     long timeStart = System.nanoTime();
     int result = bruteForce(UPPER_LIMIT);
@@ -25,15 +31,26 @@ public class Euler1 {
     System.out.println("Result (using sets): " + result + ". Completed in " + ((double)timeDelta / 1000000) + "ms.");
   }
   
+  /**
+   * Brute-force method. Will iterate through all numbers from 3 to UPPER_LIMIT, checking
+   * if each one is divisible by 3 or 5, and accumulating the value.
+   * @param limit the highest number (minus one) that the loop should reach.
+   * @return an int with the sum of all multiples.
+   */
   private static int bruteForce(int limit) {
     int result = 0;
-    for(int i = 3; i < limit; i++) {
+    for(int i = 3; i < limit; i++)
       if(i % 3 == 0 || i % 5 == 0)
         result += i;
-    }
     return result;
   }
   
+  /**
+   * Multiplicative method using sets. Will only add all numbers that are multiples of 3
+   * or five instead of iterating through the entire span.
+   * @param limit the highest number (minus one) that the loop should reach.
+   * @return an int with the sum of all multiples.
+   */
   private static int usingSets(int limit) {
     int result = 0;
     HashSet<Integer> multiples = new HashSet<Integer>();
