19b9bfd204ceb8e8e2ef6b7ff7560bf61d949d22
Tim Backus <tbackus127@gmail.com>
2015-08-31 23:22:02 -0400

    Made a smarter algorithm for finding multiples instead of iterating over all natural numbers.

--- Problem001/Euler1.java
+++ Problem001/Euler1.java
@@ -18,14 +18,18 @@ public class Euler1 {
     long timeEnd = System.nanoTime();
     long timeDelta = timeEnd - timeStart;
     System.out.println("Result (brute-force): " + result + ". Completed in " + ((double)timeDelta / 1000000) + "ms.");
+    timeStart = System.nanoTime();
+    result = usingSets(UPPER_LIMIT);
+    timeEnd = System.nanoTime();
+    timeDelta = timeEnd - timeStart;
+    System.out.println("Result (using sets): " + result + ". Completed in " + ((double)timeDelta / 1000000) + "ms.");
   }
   
   private static int bruteForce(int limit) {
     int result = 0;
     for(int i = 3; i < limit; i++) {
-      if(i % 3 == 0 || i % 5 == 0) {
+      if(i % 3 == 0 || i % 5 == 0)
         result += i;
-      }
     }
     return result;
   }
@@ -33,10 +37,12 @@ public class Euler1 {
   private static int usingSets(int limit) {
     int result = 0;
     HashSet<Integer> multiples = new HashSet<Integer>();
-    for(int i = 3; i < limit; i *= 3) {
-      if()
-    }
-    
+    for(int i = 3; i < limit; i *= 3)
+      multiples.add(i);
+    for(int i = 5; i < limit; i *= 5)
+      multiples.add(i);
+    for(int i : multiples)
+      result += i;
     return result;
   }
 }
